4bbe326cbab4004b30a0500ec908b20db06dcece
jp-knj <kenji.tomita1996@gmail.com>
2021-10-03 13:22:07 +0900

    :sparkles: setup them to watch test with jest

---
+++ lessons/02_lesson/src/no-framework/monkey-patching.js
@@ -0,0 +1,9 @@
+const assert = require('assert')
+const thumbWar = require('../thumb-war')
+const utils = require('../utils')
+
+const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
+assert.strictEqual(winner, 'Kent C. Dodds')
+
+// cleanup
+utils.getWinner = originalGetWinner
