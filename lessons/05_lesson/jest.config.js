c53b705da0184d490c20ad0538f559c901475040
jp-knj <kenji.tomita1996@gmail.com>
2021-10-04 20:15:15 +0900

    :sparkles: support using webpack css modules with jest

--- lessons/05_lesson/jest.config.js
+++ lessons/05_lesson/jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
  testEnviroment: 'jest-enviroment-jsdom',
     moduleNameMapper: {
+     '\\.module\\.css$': 'identity-obj-proxy',
      '\\.css$': require.resolve('./test/style-mock.js')
     },
 
