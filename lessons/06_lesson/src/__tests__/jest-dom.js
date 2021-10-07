7233e1de11e9d22408604231405d1e80859a7e0e
jp-knj <kenji.tomita1996@gmail.com>
2021-10-08 08:45:58 +0900

    :sparkles: use react testing libray to render and test react components

--- lessons/06_lesson/src/__tests__/jest-dom.js
+++ lessons/06_lesson/src/__tests__/jest-dom.js
@@ -1,12 +1,9 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import {getQueriesForElement} from '@testing-library/dom'
+import {render} from '@testing-library/react'
 import {FavoriteNumber} from '../favorite-number'
 
 test('renders a number input with a label "Favorite Number"', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<FavoriteNumber />, div)
-  const {getByLabelText} = getQueriesForElement(div)
+  const {getByLabelText} = render(<FavoriteNumber />)
   const input = getByLabelText(/favorite number/i)
   expect(input).toHaveAttribute('type', 'number')
 })
