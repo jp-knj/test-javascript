47b4c57ddca7be166c1beec2a3cb05d8a4184873
jp-knj <kenji.tomita1996@gmail.com>
2021-10-09 16:32:13 +0900

    test prop update with react testing library

--- lessons/06_lesson/src/__tests__/prop-updates-01.js
+++ lessons/06_lesson/src/__tests__/prop-updates-01.js
@@ -4,8 +4,13 @@ import {render} from '@testing-library/react'
 import {FavoriteNumber} from '../favorite-number'
 
 test('entering an invalid value shows an error message', () => {
-  const {getByLabelText, getByRole} = render(<FavoriteNumber />)
+  const {getByLabelText, getByRole, rerender, debug} = render(
+    <FavoriteNumber />,
+  )
   const input = getByLabelText(/favorite number/i)
   user.type(input, '10')
   expect(getByRole('alert')).toHaveTextContent(/the number is invalid/i)
+  debug()
+  rerender(<FavoriteNumber max={10} />)
+  debug()
 })
