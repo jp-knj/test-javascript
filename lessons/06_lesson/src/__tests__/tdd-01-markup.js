0c9de459d97efd0781d8ecf64366cc15343bb29d
jp-knj <kenji.tomita1996@gmail.com>
2021-10-11 08:15:45 +0900

    :sparkles: test drive assertions with dates in react

--- lessons/06_lesson/src/__tests__/tdd-01-markup.js
+++ lessons/06_lesson/src/__tests__/tdd-01-markup.js
@@ -24,6 +24,7 @@ test('renders a form with title, content, tags, and a submit button', async () =
     content: 'Test content',
     tags: ['tag1', 'tag2'],
   }
+  const preDate = new Date().getTime()
   getByLabelText(/title/i).value = fakePost.title
   getByLabelText(/content/i).value = fakePost.content
   getByLabelText(/tags/i).value = fakePost.tags.join(',')
@@ -33,9 +34,15 @@ test('renders a form with title, content, tags, and a submit button', async () =
   expect(submitButton).toBeDisabled()
   expect(mockSavePost).toHaveBeenCalledWith({
     ...fakePost,
+    date: expect.any(String),
     authorId: fakeUser.id,
   })
   expect(mockSavePost).toHaveBeenCalledTimes(1)
+
+  const postDate = new Date().getTime()
+  const date = new Date(mockSavePost.mock.calls[0][0].date).getTime()
+  expect(date).toBeGreaterThanOrEqual(preDate)
+  expect(date).toBeLessThanOrEqual(postDate)
 
   await wait(() => expect(MockRedirect).toHaveBeenCalledWith({to: '/'}, {}))
   expect(MockRedirect).toHaveBeenCalledTimes(1)
