352d7fae9394a51bbb455cba0de9788a8bdd6331
jp-knj <kenji.tomita1996@gmail.com>
2021-10-10 15:38:28 +0900

    :sparkles: test drive the submission of a react form with react testing library

--- lessons/06_lesson/src/__tests__/tdd-01-markup.js
+++ lessons/06_lesson/src/__tests__/tdd-01-markup.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {render} from '@testing-library/react'
+import {render, fireEvent} from '@testing-library/react'
 import {Editor} from '../post-editor-01-markup'
 
 test('renders a form with title, content, tags, and a submit button', () => {
@@ -7,5 +7,8 @@ test('renders a form with title, content, tags, and a submit button', () => {
   getByLabelText(/title/i)
   getByLabelText(/content/i)
   getByLabelText(/tags/i)
-  getByText(/submit/i)
+  const submitButton = getByText(/submit/i)
+
+  fireEvent.click(submitButton)
+  expect(submitButton).toBeDisabled()
 })
--- lessons/06_lesson/src/post-editor-01-markup.js
+++ lessons/06_lesson/src/post-editor-01-markup.js
@@ -1,15 +1,23 @@
 import React from 'react'
 
 function Editor() {
+  const [isServing, setIsSaving] = React.useState(false)
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    setIsSaving(true)
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label htmlFor="title-input">Title</label>
       <input id="title-input" />
       <label htmlFor="content-input">content</label>
       <input id="content-input" />
       <label htmlFor="tags-input">tags</label>
       <input id="tags-input" />
-      <button type="submit">Submit</button>
+      <button type="submit" disabled={isServing}>
+        Submit
+      </button>
     </form>
   )
 }
