a4abf1774c4491eaa63682a596b8e61d7f5bbd89
jp-knj <kenji.tomita1996@gmail.com>
2021-10-10 16:06:41 +0900

    :sparkles: test drive the api call of a react form with react testing library

--- lessons/06_lesson/src/__tests__/tdd-01-markup.js
+++ lessons/06_lesson/src/__tests__/tdd-01-markup.js
@@ -1,14 +1,32 @@
 import React from 'react'
 import {render, fireEvent} from '@testing-library/react'
+import {savePost as mockSavePost} from '../api'
 import {Editor} from '../post-editor-01-markup'
 
+jest.mock('../api')
+
+afterEach(() => {
+  jest.clearAllMocks()
+})
 test('renders a form with title, content, tags, and a submit button', () => {
-  const {getByLabelText, getByText} = render(<Editor />)
-  getByLabelText(/title/i)
-  getByLabelText(/content/i)
-  getByLabelText(/tags/i)
+  mockSavePost.mockResolvedValueOnce()
+  const fakeUser = {id: 'user-1'}
+  const {getByLabelText, getByText} = render(<Editor user={fakeUser} />)
+  const fakePost = {
+    title: 'Test title',
+    content: 'Test content',
+    tags: ['tag1', 'tag2'],
+  }
+  getByLabelText(/title/i).value = fakePost.title
+  getByLabelText(/content/i).value = fakePost.content
+  getByLabelText(/tags/i).value = fakePost.tags.join(',')
   const submitButton = getByText(/submit/i)
 
   fireEvent.click(submitButton)
   expect(submitButton).toBeDisabled()
+  expect(mockSavePost).toHaveBeenCalledWith({
+    ...fakePost,
+    authorId: fakeUser.id,
+  })
+  expect(mockSavePost).toHaveBeenCalledTimes(1)
 })
--- lessons/06_lesson/src/post-editor-01-markup.js
+++ lessons/06_lesson/src/post-editor-01-markup.js
@@ -1,20 +1,29 @@
 import React from 'react'
+import {savePost} from './api'
 
-function Editor() {
+function Editor({user}) {
   const [isServing, setIsSaving] = React.useState(false)
 
   function handleSubmit(e) {
     e.preventDefault()
+    const {title, content, tags} = e.target.elements
+    const newPost = {
+      title: title.value,
+      content: content.value,
+      tags: tags.value.split(',').map(t => t.trim()),
+      authorId: user.id,
+    }
     setIsSaving(true)
+    savePost(newPost)
   }
   return (
     <form onSubmit={handleSubmit}>
       <label htmlFor="title-input">Title</label>
-      <input id="title-input" />
+      <input id="title-input" name="title" />
       <label htmlFor="content-input">content</label>
-      <input id="content-input" />
+      <input id="content-input" name="content" />
       <label htmlFor="tags-input">tags</label>
-      <input id="tags-input" />
+      <input id="tags-input" name="tags" />
       <button type="submit" disabled={isServing}>
         Submit
       </button>
