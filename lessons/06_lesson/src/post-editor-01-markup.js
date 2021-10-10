81eb6465f61a1520a67f872b54befe7365da628c
jp-knj <kenji.tomita1996@gmail.com>
2021-10-10 14:50:53 +0900

    :tada: test driven the development of a react form with react testing libary

--- lessons/06_lesson/src/post-editor-01-markup.js
+++ lessons/06_lesson/src/post-editor-01-markup.js
@@ -5,13 +5,10 @@ function Editor() {
     <form>
       <label htmlFor="title-input">Title</label>
       <input id="title-input" />
-
-      <label htmlFor="content-input">Content</label>
-      <textarea id="content-input" />
-
-      <label htmlFor="tags-input">Tags</label>
+      <label htmlFor="content-input">content</label>
+      <input id="content-input" />
+      <label htmlFor="tags-input">tags</label>
       <input id="tags-input" />
-
       <button type="submit">Submit</button>
     </form>
   )
