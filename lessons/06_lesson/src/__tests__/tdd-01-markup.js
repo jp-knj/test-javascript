54c4e89883cfb786301c0f1c056dcda1a27d70ea
jp-knj <kenji.tomita1996@gmail.com>
2021-10-12 08:20:11 +0900

    use generated data in tests with tests-data-bot to improve test maintainability

--- lessons/06_lesson/src/__tests__/tdd-01-markup.js
+++ lessons/06_lesson/src/__tests__/tdd-01-markup.js
@@ -1,37 +1,47 @@
 import React from 'react'
 import {render, fireEvent, wait} from '@testing-library/react'
+import {build, fake, sequence} from 'test-data-bot'
 import {Redirect as MockRedirect} from 'react-router'
 import {savePost as mockSavePost} from '../api'
-import {Editor} from '../post-editor-01-markup'
+import {Editor} from '../post-editor-06-generate-data'
 
 jest.mock('react-router', () => {
   return {
     Redirect: jest.fn(() => null),
   }
 })
-
 jest.mock('../api')
 
 afterEach(() => {
   jest.clearAllMocks()
 })
+
+const postBuilder = build('Post').fields({
+  title: fake(f => f.lorem.words()),
+  content: fake(f => f.lorem.paragraphs().replace(/\r/g, '')),
+  tags: fake(f => [f.lorem.word(), f.lorem.word(), f.lorem.word()]),
+})
+
+const userBuilder = build('User').fields({
+  id: sequence(s => `user-${s}`),
+})
+
 test('renders a form with title, content, tags, and a submit button', async () => {
   mockSavePost.mockResolvedValueOnce()
-  const fakeUser = {id: 'user-1'}
+  const fakeUser = userBuilder()
   const {getByLabelText, getByText} = render(<Editor user={fakeUser} />)
-  const fakePost = {
-    title: 'Test title',
-    content: 'Test content',
-    tags: ['tag1', 'tag2'],
-  }
+  const fakePost = postBuilder()
   const preDate = new Date().getTime()
+
   getByLabelText(/title/i).value = fakePost.title
   getByLabelText(/content/i).value = fakePost.content
-  getByLabelText(/tags/i).value = fakePost.tags.join(',')
+  getByLabelText(/tags/i).value = fakePost.tags.join(', ')
   const submitButton = getByText(/submit/i)
 
   fireEvent.click(submitButton)
+
   expect(submitButton).toBeDisabled()
+
   expect(mockSavePost).toHaveBeenCalledWith({
     ...fakePost,
     date: expect.any(String),
@@ -45,5 +55,4 @@ test('renders a form with title, content, tags, and a submit button', async () =
   expect(date).toBeLessThanOrEqual(postDate)
 
   await wait(() => expect(MockRedirect).toHaveBeenCalledWith({to: '/'}, {}))
-  expect(MockRedirect).toHaveBeenCalledTimes(1)
 })
