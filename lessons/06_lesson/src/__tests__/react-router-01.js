a3f797638b9c9192b3c864a9b834b9f03f1a6367
jp-knj <kenji.tomita1996@gmail.com>
2021-10-13 21:26:26 +0900

    :sparkles: create a custom render function to simplify tests of react-router components

--- lessons/06_lesson/src/__tests__/react-router-01.js
+++ lessons/06_lesson/src/__tests__/react-router-01.js
@@ -1,29 +1,33 @@
 import React from 'react'
 import {Router} from 'react-router-dom'
 import {createMemoryHistory} from 'history'
-import {render, fireEvent} from '@testing-library/react'
+import {render as rtlRender, fireEvent} from '@testing-library/react'
 import {Main} from '../main'
 
+function render(
+  ui,
+  {
+    route = '/',
+    history = createMemoryHistory({initialEntries: [route]}),
+    ...renderOptions
+  } = {},
+) {
+  function Wrapper({children}) {
+    return <Router history={history}>{children}</Router>
+  }
+  return {...rtlRender(ui, {wrapper: Wrapper, ...renderOptions}), history}
+}
+
 test('main renders about, home and I can navigate to those pages', () => {
-  const history = createMemoryHistory({initialEntries: ['/']})
-  const {getByRole, getByText} = render(
-    <Router history={history}>
-      <Main />
-    </Router>,
-  )
+  const {getByRole, getByText} = render(<Main />)
   expect(getByRole('heading')).toHaveTextContent(/home/i)
   fireEvent.click(getByText(/about/i))
   expect(getByRole('heading')).toHaveTextContent(/about/i)
 })
 
 test('loading on a bad page shows no match component', () => {
-  const history = createMemoryHistory({
-    initialEntries: ['/something-that-does-not-match'],
+  const {getByRole} = render(<Main />, {
+    route: '/something-that-does-not-match',
   })
-  const {getByRole} = render(
-    <Router history={history}>
-      <Main />
-    </Router>,
-  )
   expect(getByRole('heading')).toHaveTextContent(/404/i)
 })
