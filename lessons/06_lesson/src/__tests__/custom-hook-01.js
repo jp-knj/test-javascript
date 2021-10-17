0b97f3ff3b012fc0a08632c5014c0fee6c64749d
jp-knj <kenji.tomita1996@gmail.com>
2021-10-17 22:53:53 +0900

    test updates to custom react hook with rerender from react hooks testing library

--- lessons/06_lesson/src/__tests__/custom-hook-01.js
+++ lessons/06_lesson/src/__tests__/custom-hook-01.js
@@ -14,3 +14,13 @@ test('allow customization of the initial count', () => {
   const {result} = renderHook(useCounter, {initialProps: {initialCount: 3}})
   expect(result.current.count).toBe(3)
 })
+
+test('the step can be changed', () => {
+  const {result, rerender} = renderHook(useCounter, {initialProps: {step: 3}})
+  expect(result.current.count).toBe(0)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(3)
+  rerender({step: 2})
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(1)
+})
