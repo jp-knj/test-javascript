01c14c6db25ca5d321cd6f06881a3edb6f7a729a
jp-knj <kenji.tomita1996@gmail.com>
2021-10-18 19:46:54 +0900

    :sparkles: test unmounting a react component with react testing library

--- lessons/06_lesson/src/__tests__/unmounting.js
+++ lessons/06_lesson/src/__tests__/unmounting.js
@@ -1,20 +1,16 @@
 import React from 'react'
 import {render, act} from '@testing-library/react'
 import {Countdown} from '../countdown'
-
 beforeAll(() => {
   jest.spyOn(console, 'error').mockImplementation(() => {})
 })
-
 afterAll(() => {
   console.error.mockRestore()
 })
-
 afterEach(() => {
   jest.clearAllMocks()
   jest.useRealTimers()
 })
-
 test('does not attempt to state when unmounted to prevent memory leaks', () => {
   jest.useFakeTimers()
   const {unmount} = render(<Countdown />)
