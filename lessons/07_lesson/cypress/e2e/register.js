cc6fe76585d1023a9c9f8cd08064a1a79e405c50
jp-knj <kenji.tomita1996@gmail.com>
2021-10-24 10:29:12 +0900

    :sparkles: Test user registertion with cypress

---
+++ lessons/07_lesson/cypress/e2e/register.js
@@ -0,0 +1,20 @@
+describe('registration', () => {
+    it('should register a new user', () => {
+        const user = { username: 'bob', password: 'yo'}
+
+        cy.visit('/')
+            .findByText('register/i')
+            .click()
+            .findByLabelText(/username/i)
+            .type(user.username)
+            .findByLabelText(/password/i)
+            .type(user.password)
+            .findByText(/submit/i)
+            .click()
+            .url()
+            .should('eq', `${Cypress.config().baseUrl}/`)
+            .window()
+            .its('localStorage.token')
+            .should('be.a', 'string')
+    })
+})
