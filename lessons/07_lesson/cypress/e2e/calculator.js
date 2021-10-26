c024037f1451e1dc8a05790960d090b2c25f1a65
jp-knj <kenji.tomita1996@gmail.com>
2021-10-27 08:40:48 +0900

    :sparkles: run tests as an Authenticated user with cypress

---
+++ lessons/07_lesson/cypress/e2e/calculator.js
@@ -0,0 +1,37 @@
+describe('anonymous calculator', () => {
+    it('can make calculations', () => {
+        cy.visit('/')
+            .findByText(/^1$/)
+            .click()
+            .findByText(/^\+$/)
+            .click()
+            .findByText(/^2$/)
+            .click()
+            .findByText(/^=$/)
+            .click()
+            .findByTestId('total')
+            .should('have.text', '3')
+    })
+})
+
+describe('authenticated calculator', () => {
+    it('displays the username', () => {
+        cy.createUser().then(user => {
+            cy.visit('/')
+                .findByText(/login/i)
+                .click()
+                .findByLabelText(/username/i)
+                .type(user.username)
+                .findByLabelText(/password/i)
+                .type(user.password)
+                .findByText(/submit/i)
+                .click()
+                .findByTestId('username-display')
+                .should('have.text', user.username)
+                .findByText(/logout/i)
+                .click()
+                .findByTestId('username-display')
+                .should('not.exist')
+        })
+    })
+})
