b913150c64ff1c28158d9c5796bdb138cfba8fe0
jp-knj <kenji.tomita1996@gmail.com>
2021-11-10 08:47:36 +0900

    :+1: #12 create a pre-configured axios client for the baseURL

--- lessons/08_lesson/src/__tests__/auth.exercise.js
+++ lessons/08_lesson/src/__tests__/auth.exercise.js
@@ -14,11 +14,12 @@ afterAll(async () => server.close())
 
 beforeEach(() => resetDb())
 
+const baseURL = 'http://localhost:8000/api/';
+const api = axios.create({baseURL})
 test('auth flow', async () => {
     const {username, password} = generate.loginForm()
 
-    // register
-    const rResult = await axios.post('http://localhost:8000/api/auth/register', {
+    const rResult = await api.post(`auth/register`, {
         username,
         password,
     })
@@ -28,14 +29,13 @@ test('auth flow', async () => {
         username,
     })
 
-    // login
-    const lResult = await axios.post('http://localhost:8000/api/auth/login', {
+    const lResult = await api.post(`auth/login`, {
         username,
         password,
     })
     expect(lResult.data.user).toEqual(rResult.data.user)
 
-    const mResult = await axios.get('http://localhost:8000/api/auth/me', {
+    const mResult = await api.get(`auth/me`, {
         headers: {
             Authorization: `Bearer ${lResult.data.user.token}`,
         }
