6c6eeddad29d3144daf63e98d6913c2dffd5b3df
jp-knj <kenji.tomita1996@gmail.com>
2021-11-19 08:45:32 +0900

    :art: #15 support importing css files with jest modulenamemapper

--- calculator/src/shared/__tests__/auto-scaling-text.js
+++ calculator/src/shared/__tests__/auto-scaling-text.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {render} from '@testing-library/react'
-import AutoScalingText from 'auto-scaling-text'
+import AutoScalingText from '../auto-scaling-text'
 
 test('renders', () => {
     render(<AutoScalingText/>)
